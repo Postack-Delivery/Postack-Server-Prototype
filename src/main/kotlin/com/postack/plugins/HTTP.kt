4c32ed743ff89f4aee8b96a12a919457625af02f
Mwai Banda <mwai.banda@cbn.org>
2022-12-19 17:06:57 -0600

    removed Hsts

--- src/main/kotlin/com/postack/plugins/HTTP.kt
+++ src/main/kotlin/com/postack/plugins/HTTP.kt
@@ -1,17 +1,12 @@
 package com.postack.plugins
 
 import io.ktor.http.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.plugins.hsts.*
-import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.http.content.*
-import io.ktor.server.plugins.compression.*
-import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
     install(CORS) {
